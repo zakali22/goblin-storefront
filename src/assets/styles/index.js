1c9e2e6480ffefa6ee5dc00bd86fce7fb1bdfe9f
Zak Ali <zak.ali@thetin.local>
2021-08-14 20:19:24 +0100

    Add base styling

---
+++ src/assets/styles/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+// Layout
+export const Container = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 10px;
+`
+
+// Elements
+export const Logo = styled.h1`
+    color: #4aa52e
+`
+
