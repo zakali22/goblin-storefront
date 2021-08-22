d36d237490cfcf9e5ec4c4139e5e90e47568aa99
Zak Ali <zak.ali@thetin.local>
2021-08-22 20:07:04 +0100

    Dynamically render the product list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
+import axios from "axios"
 import GlobalStyles from "./assets/styles/globalStyles"
 import Layout from "./components/Layout"
 import ProductListing from "./components/Products/ProductListing"
 import CartSummary from "./components/Cart/CartSummary"
 import Checkout from "./components/Checkout/Checkout"
 import OrderSummary from "./components/Checkout/OrderSummary"
+import { useAppState } from './utils/useAppState';
+import {setProducts, setCategories} from "./state/appStateActions"
 
 function App() {
+  const {dispatch} = useAppState()
+  
+  const fetchData = async () => {
+    const res = await axios.get("http://localhost:4000/products")
+    return res.data
+  }
+
+  useEffect(() => {
+    fetchData().then(res => {
+      const {categories} = res
+      const categoriesList = categories.map(({name}:{name: string}) => ({name})) 
+      const productList = categories.map(({items}: {items: any}) => (items)).flat(Infinity)
+
+      dispatch(setProducts(productList))
+      dispatch(setCategories(categoriesList))
+    }).catch(err => {
+      console.log(err)
+      return err
+    })
+  }, [dispatch])
+
   return (
     <Fragment>
       <GlobalStyles />
--- src/components/Products/ProductListing.tsx
+++ src/components/Products/ProductListing.tsx
@@ -1,20 +1,18 @@
+import {useAppState} from "../../utils/useAppState"
 import {GridListing} from "../../assets/styles/index"
 import WrapperContainer from "../WrapperContainer"
 import ProductCard from "./ProductCard"
 
-const ProductListing = () => (
-    <WrapperContainer title="Weapons">
-        <GridListing gridColumnWidth={200} padding="0">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-        </GridListing>
-    </WrapperContainer>
-)
+const ProductListing = () => {
+    const {state: {productList}} = useAppState()
+
+    return (
+        <WrapperContainer title="All products">
+            <GridListing gridColumnWidth={200} padding="0">
+                {productList.length > 0 ? (productList.map(product => <ProductCard product={product}/>)) : null}
+            </GridListing>
+        </WrapperContainer>
+   )
+}
 
 export default ProductListing
