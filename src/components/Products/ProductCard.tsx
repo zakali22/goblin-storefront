967ec8551ecd3194c29ca513a94fd7efee80d51b
Zak Ali <zak.ali@thetin.local>
2021-08-23 22:19:43 +0100

    Add item to cart

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,20 +1,26 @@
 import {Card} from "../../assets/styles/components/Card"
 import {ImageWrapper} from "../../assets/styles/index"
 import {Product} from "../../state/appStateReducer"
+import {addProductCart} from "../../state/appStateActions"
+import {useAppState} from "../../utils/useAppState"
 
 type ProductCardProps = {
     product: Product
 }
 
-const ProductCard = ({product: {_id, image, name, price, description}}: ProductCardProps) => (
-    <Card className="nes-container is-rounded">
-        <ImageWrapper width="64">
-            <img src={image} alt="Product"/>
-        </ImageWrapper>
-        <p>{name}</p>
-        <p>{price} Zm</p>
-        <button type="button" className="nes-btn is-primary">Add to card</button>
-    </Card>
-)
+const ProductCard = ({product}: ProductCardProps) => {
+    const {dispatch} = useAppState()
+
+    return (
+        <Card className="nes-container is-rounded">
+            <ImageWrapper width="64">
+                <img src={product.image} alt="Product"/>
+            </ImageWrapper>
+            <p>{product.name}</p>
+            <p>{product.price} Zm</p>
+            <button type="button" className="nes-btn is-primary" onClick={() => dispatch(addProductCart(product))}>Add to card</button>
+        </Card>
+    )
+}
 
 export default ProductCard
