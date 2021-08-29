15871d76f1ee4af4889faf113ca13aa96f304a03
Zak Ali <zak.ali@thetin.local>
2021-08-29 17:54:16 +0100

    Add cart total

--- src/components/Cart/CartSummary.tsx
+++ src/components/Cart/CartSummary.tsx
@@ -1,20 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useAppState } from "../../utils/useAppState"
+import {Product} from "../../state/appStateReducer"
 import WrapperContainer from "../WrapperContainer"
 import CartListing from "./CartListing"
 import CartTotal from "./CartTotal"
 
 const CartSummary = () => {
     const {state: {cart}} = useAppState()
+    const [cartTotal, setCartTotal] = useState(0)
 
     useEffect(() => {
+        const cartTotal = (cart as Array<Product[] | []>).reduce((total: any, amount: any) => {
+            return total + Number(amount.price)
+        }, 0)
 
+        setCartTotal(cartTotal)
     }, [cart])
 
     return (
         <WrapperContainer title="Cart summary" centered>
             <CartListing cart={cart} />
-            {/* <CartTotal cartTotal={} /> */}
+            <CartTotal cartTotal={cartTotal} />
             <button type="button" className="nes-btn is-primary">Go to checkout</button>
         </WrapperContainer>
     )
